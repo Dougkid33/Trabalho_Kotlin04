f2fde20d85d01c9800640319db04e4863017476f
Dougkid33 <douglasaugustomsilva@hotmail.com>
2023-10-22 19:39:27 -0300

    commit final

--- app/src/main/java/com/example/trabalhoex04/MainActivity.kt
+++ app/src/main/java/com/example/trabalhoex04/MainActivity.kt
@@ -1,11 +1,27 @@
-package com.example.trabalhoex04
-
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.trabalhoex04.R
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tvMatrix = findViewById<TextView>(R.id.tv_matrix)
+        val btnStart = findViewById<Button>(R.id.btn_start)
+
+        btnStart.setOnClickListener {
+            val matrix = Array(12) { IntArray(13) { Random.nextInt(1, 100) } }
+            val resultMatrix = matrix.map { row ->
+                val max = row.maxOrNull() ?: 1
+                row.map { it / max.toDouble() }.toDoubleArray()
+            }
+
+            tvMatrix.text = resultMatrix.joinToString("\n") { it.joinToString(" ") }
+        }
     }
 }
